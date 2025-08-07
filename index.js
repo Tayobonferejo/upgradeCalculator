e1c4284dc7e0e7773b6f0253b4079fbd46ff05ba
tayobonferejo <oluwatayogeorgeige@gmail.com>
2025-08-07 14:34:59 +0100

    js functionality begins

--- index.js
+++ index.js
@@ -4,4 +4,4 @@
  const deleteButton = document.querySelector('[data-delete]');
  const allClearButton = document.querySelector('[data-all-clear]');
  const previousOperandTextElement = document.querySelector('[data-previous-operand]');
- const currentOperandTextElement = document.querySelector('data-current-opreand]');
+ const currentOperandTextElement = document.querySelector('[data-current-operand]');
