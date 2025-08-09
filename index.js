f0781d604939b3415b7f15d393c739d0886809f3
tayobonferejo <oluwatayogeorgeige@gmail.com>
2025-08-09 13:25:26 +0100

    fixed error in constructor

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
+ class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+        this.clear();
+    };
+
+   clear() {
+      this.currentOperand = ""
+      this.previousOperand = ""
+      this.operation = undefined;
+   }
+
+   //  delete() {
+
+   //  }
+
+   //  appendNumber(number) {
+
+   //  };
+
+   //  chooseOperation(operation) {
+
+   //  };
+
+   //  compute() {
+
+   //  }
+
+   //  updateDisplay() {
+
+   //  };
+
+ }
+ 
  const numberButtons = document.querySelectorAll('[data-number]');
  const operationButtons = document.querySelectorAll('[data-operation]');
  const equalsButton =  document.querySelector('[data-equals]');
@@ -5,3 +40,12 @@
  const allClearButton = document.querySelector('[data-all-clear]');
  const previousOperandTextElement = document.querySelector('[data-previous-operand]');
  const currentOperandTextElement = document.querySelector('[data-current-operand]');
+
+ const calculator = new calculator(previousOperandTextElement, currentOperandTextElement)
+
+ numberButtons.forEach( button => {
+    button.addEventListener("click", () => {
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay()
+    })
+ })
